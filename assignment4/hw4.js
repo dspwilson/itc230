e5844e690a53a02bdcb42cc468ed5591338776ca
Derek Wilson <dspwilson@ohanapecosh.com>
2017-05-08 09:13:52 -0700

    Add files via upload

---
+++ assignment4/hw4.js
@@ -0,0 +1,9 @@
+// eslint practice file for ITC 230
+//names = ['sara','joe','dave','ann']
+let names = ['sara','joe','dave','ann'];
+names.map( (item) => {
+ return item.toUpperCase();
+ // x = 2;
+});
+// let x = 2;
+
