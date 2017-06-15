1fd6e86e763fb190bfd5b8ac2dc0151d7331c041
Derek Wilson <dspwilson@ohanapecosh.com>
2017-06-15 11:15:24 -0700

    Add files via upload

---
+++ assignment6/lib/credentials.js
@@ -0,0 +1,19 @@
+/*-----------------------------------
+/  credentials.js
+/  Derek S Wilson
+/  May 22, 2017
+/  Seattle Central College
+/  ITC230 Advanced JavaScript
+/  Assignment 6
+/-----------------------------------*/
+
+
+exports.mongo = {
+   development: {
+      connectionString: 'mongodb://dspwilson:ITC230@ds137801.mlab.com:37801/seattle-public-art',
+    },
+    production: {
+      connectionString: 'mongodb://dspwilson:ITC230@ds137801.mlab.com:37801/seattle-public-art',
+    }
+};
+
